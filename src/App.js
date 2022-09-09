f00bb5697d78d133d166c3a034280bb6e72bbe9d
Sushant Prasad <sushantprasad1234@gmail.com>
2022-09-09 18:45:04 +0530

    committed

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) amount = 1;
+    if (count > 8) amount = 8;
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className="section-center">
+      <h1>Lorem ipsum generator</h1>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((para, index) => {
+          return <p key={index}>{para}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+export default App;
